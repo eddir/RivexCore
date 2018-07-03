93732422c574d85f3aa7ea4ded8848624e34fe34
eddir <eddirworkmail@gmail.com>
2018-07-03 23:34:28 +0300

    Пропали классы к предыдущему коммиту

---
+++ src/rivex/rivexcore/command/EntityKill.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eddir
+ * Date: 7/3/18
+ * Time: 8:47 PM
+ */
+
+namespace rivex\rivexcore\command;
+
+use pocketmine\command\CommandSender;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\Player;
+
+use rivex\rivexcore\Main;
+
+class EntityKill extends RivexCommand
+{
+
+    public function __construct(Main $main)
+    {
+        parent::__construct($main, "entitykill", "Kill the entity by damage them",
+            "/entitykill", "entitykill", ["ekill"]);
+        $this->setPermission("rivex.command.ekill");
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @param string $commandLabel
+     * @param string[] $args
+     *
+     * @return mixed
+     */
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if ($sender instanceof Player) {
+            $this->getMain()->getEvents()->callback("EntiryDamageByEntityEvent",
+                function (EntityDamageByEntityEvent $event, array $args) {
+                    $damager = $event->getDamager();
+                    /** @var Player $player */
+                    $player = $args[0];
+                    if ($damager instanceof Player and $damager->getName() == $player->getName()) {
+                        $event->getEntity()->kill();
+                        return true;
+                    }
+                    return false;
+                }, $sender
+            );
+            $sender->sendMessage("§aУдарьте моба для его исчезновения.");
+        } else {
+            if (isset($args[0])) {
+                $entity = $this->getMain()->getServer()->getDefaultLevel()->getEntity($args[0]);
+                if ($entity !== null) {
+                    $entity->kill();
+                    $sender->sendMessage("Моб убит");
+                } else {
+                    $sender->sendMessage("Не найден моб с таким id");
+                }
+            } else {
+                $sender->sendMessage("Необходимо указывать id");
+            }
+        }
+        return true;
+    }
+}
---
+++ src/rivex/rivexcore/listener/CallbackListener.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eddir
+ * Date: 7/3/18
+ * Time: 8:34 PM
+ */
+
+namespace rivex\rivexcore\listener;
+
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\event\Listener;
+use rivex\rivexcore\Main;
+
+class CallbackListener implements Listener
+{
+
+    private $callbacks = array(
+        "EntityDamageByEntityEvent" => array()
+    );
+
+    private $main;
+
+    public function __construct(Main $main)
+    {
+        $this->main = $main;
+    }
+
+    public function callback(string $event, callable $function, ...$args)
+    {
+        $this->callbacks[$event][] = array($function, $args);
+    }
+
+    /**
+     * @param EntityDamageByEntityEvent $event
+     */
+    public function onDamage(EntityDamageByEntityEvent $event)
+    {
+        foreach ($this->callbacks["EntityDamageByEntityEvent"] as $key => $callback) {
+            if ($callback[0]($event, $callback[1])) {
+                unset($this->callbacks["EntityDamageByEntityEvent"][$key]);
+            }
+        }
+    }
+
+    /**
+     * @return Main
+     */
+    public function getMain(): Main
+    {
+        return $this->main;
+    }
+
+}
