334bd32babb80418931bfb5ca890984208c5b447
eddir <eddirworkmail@gmail.com>
2018-09-02 19:06:58 +0300

    Список серверов в меню теперь настраивается в конфиге

--- src/rivex/rivexcore/modules/window/primal/ServersWindow.php
+++ src/rivex/rivexcore/modules/window/primal/ServersWindow.php
@@ -24,13 +24,9 @@ class ServersWindow extends BaseWindow implements Window
     public function __construct($id)
     {
         $this->ui = new Menu("§9Выберите сервер");
-        $port = Main::getInstance()->getServer()->getPort();
-        if ($port == 19132 or $port == 19134) {
-            $this->ui->addButton(new Button("§2Хаб §e(Вы здесь)"));
-            $this->ui->addButton(new Button("§2Космическое выживание"));
-        } else {
-            $this->ui->addButton(new Button("§2Хаб"));
-            $this->ui->addButton(new Button("§2Космическое выживание §e(Вы здесь)"));
+        $servers = Main::getInstance()->getConfig()->get("servers");
+        foreach ($servers as $server) {
+            $this->ui->addButton(new Button($server['name']));
         }
         parent::__construct($id, 'servers');
     }
@@ -47,28 +43,16 @@ class ServersWindow extends BaseWindow implements Window
 
     public function handle(Player $player, $response)
     {
-        $port = Main::getInstance()->getServer()->getPort();
-        switch ($response) {
-            case 0:
-                if ($port == 19132 or $port == 19134) {
-                    $player->sendMessage("§bВы уже находитесь на этом сервере!");
-                } else {
-                    $pk = new TransferPacket();
-                    $pk->address = "rivex-serv.ru";
-                    $pk->port = 19132;
-                    $player->dataPacket($pk);
-                }
-                break;
-            case 1:
-                if ($port == 19132 or $port == 19134) {
-                    $pk = new TransferPacket();
-                    $pk->address = "rivex-serv.ru";
-                    $pk->port = 19131;
-                    $player->dataPacket($pk);
-                } else {
-                    $player->sendMessage("§bВы уже находитесь на этом сервере!");
-                }
-                break;
+        $servers = Main::getInstance()->getConfig()->get("servers");
+        if (isset($servers[$response])) {
+            if ($servers[$response]['transfer'] === true) {
+                $pk = new TransferPacket();
+                $pk->address = $servers[$response]['ip'];
+                $pk->port = $servers[$response]['port'];
+                $player->dataPacket($pk);
+            } else {
+                $player->sendMessage("§bВы уже находитесь на этом сервере!");
+            }
         }
         return true;
     }
